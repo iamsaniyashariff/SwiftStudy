1896226cb333798e23f588fabae2b344dbba791d
Saniya Shariff <97965761+iamsaniyashariff@users.noreply.github.com>
2024-01-21 19:53:55 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import MindMap from './components/MindMap';
+
+
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1>Study App</h1>
+      <MindMap />
+      <TextEditor />
+    </div>
+  );
+};
+
+export default App;
